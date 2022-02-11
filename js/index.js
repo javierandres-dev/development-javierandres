662143ca7642a88ea28797c740985f598df6b683
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2022-02-10 23:12:08 -0500

    [last update] 20220210

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-console.log('Works!');
+'use strict';
+const lang = window.navigator.language,
+  d = window.document;
+let texts = null;
+
+const $profession = d.getElementById('profession'),
+  $greet = d.getElementById('greet'),
+  $i = d.getElementById('i'),
+  $aboutMe = d.getElementById('aboutMe'),
+  $me = d.getElementById('me'),
+  $contactMe = d.getElementById('contactMe'),
+  $copyright = d.getElementById('copyright');
+
+d.addEventListener('DOMContentLoaded', () => {
+  setTexts();
+});
+
+const setTexts = async () => {
+  let res = await fetch('../json/index.json');
+  res = await res.json();
+  lang.startsWith('es') ? (texts = res.es) : (texts = res.en);
+
+  let html = '';
+  texts.me.forEach((item) => {
+    html += `<p>${item}</p>`;
+  });
+
+  $profession.textContent = texts.profession;
+  $greet.textContent = texts.greet;
+  $i.textContent = texts.i;
+  $aboutMe.textContent = texts.aboutMe;
+  $me.innerHTML = html;
+  $contactMe.textContent = texts.contactMe;
+  $copyright.textContent = texts.copyright;
+};
